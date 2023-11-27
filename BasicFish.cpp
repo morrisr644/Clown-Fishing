3b7fa86c5874b140c51b2d63f589add851912b37
Rebecca Morris <morrisr644@lynchburg.edu>
2023-11-27 09:21:30 -0500

    comments

--- BasicFish.cpp
+++ BasicFish.cpp
@@ -178,7 +178,7 @@ void BasicFish::FixCollisions() // pulled from Madhav FPSActor
 
 	Vector3 currentPos = this->GetPosition();
 
-	if (isOnLine && currentPos.y <= 300.0)
+	if (isOnLine && currentPos.y <= 300.0) // This is here so the fish get caught a bit earlier than intersecting with the wall
 	{
 		// If the fish collides with any of the walls, the player is no longer reeling it in
 		// It either got away or was caught
